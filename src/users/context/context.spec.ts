b0542bbd92dee97ebb55fb6869b9ce9ec3cf891d
Muhammad Naufal Rachfian dJ <naufalrdj@gmail.com>
2023-07-30 21:59:20 +0700

    feat: add RequestContext unit-testing

---
+++ src/users/context/context.spec.ts
@@ -0,0 +1,28 @@
+import { ExecutionContext } from '@nestjs/common';
+import { RequestContext } from './context';
+
+describe('RequestContext Decorator', () => {
+  it('should return the request object from ExecutionContext', () => {
+    const mockRequest = { headers: { Authorization: 'Bearer token' }, body: { data: 'test' } };
+
+    const mockExecutionContext: ExecutionContext = {
+        switchToHttp: jest.fn(() => ({
+          getRequest: jest.fn().mockReturnValue(mockRequest),
+          getResponse: jest.fn(),
+          getNext: jest.fn()
+        })),
+        switchToRpc: jest.fn(),
+        switchToWs: jest.fn(),
+        getClass: jest.fn(),
+        getHandler: jest.fn(),
+        getArgs: jest.fn(),
+        getArgByIndex: jest.fn(),
+        getType: jest.fn(),
+      };
+
+    const result = RequestContext(mockRequest, mockExecutionContext as ExecutionContext);
+
+    // Expect the result to be the same as the mockRequest
+    expect(jest.spyOn(mockExecutionContext.switchToHttp(), 'getRequest')).toHaveBeenCalledTimes(0);
+  });
+});
