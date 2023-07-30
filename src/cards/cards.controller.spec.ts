34222c5531d9091b86c31b1d41e563015c1361a8
Muhammad Naufal Rachfian dJ <naufalrdj@gmail.com>
2023-07-30 20:06:33 +0700

    feat: add CardService & CardController unit-test

--- src/cards/cards.controller.spec.ts
+++ src/cards/cards.controller.spec.ts
@@ -1,18 +1,132 @@
+import { PassportModule } from '@nestjs/passport';
 import { Test, TestingModule } from '@nestjs/testing';
+import { User } from '../users/schemas/user.schema';
 import { CardsController } from './cards.controller';
+import { CardsService } from './cards.service';
+import { CreateCardDTO } from './dto/createCard.dto';
+import { Category } from './schemas/card.schema';
+import { UpdateCardDTO } from './dto/updateCard.dto';
+import { ObjectId } from 'mongodb';
 
 describe('CardsController', () => {
-  let controller: CardsController;
+  let cardService: CardsService;
+  let cardController: CardsController;
+
+  const id = '64c2c16a2a06d43ed9ada952';
+
+  const myObjectId: ObjectId = ObjectId.createFromHexString(id);
+
+  const mockUser = {
+    _id: myObjectId,
+    username: 'NaufalRDJ',
+    password: 'xcvg5hjbn1237',
+  };
+
+  const mockCard = {
+    _id: '64c2c1802a06d43ed9ada955',
+    name: 'Quantum Physics',
+    status: true,
+    content: 'Lorem ipsum dolor sit amet',
+    category: Category.PHYSICS,
+    user: '64c2c16a2a06d43ed9ada952',
+  };
+
+  const mockCardService = {
+    findAll: jest.fn().mockResolvedValueOnce([mockCard]),
+    create: jest.fn(),
+    findById: jest.fn().mockResolvedValueOnce(mockCard),
+    updateById: jest.fn(),
+    deleteById: jest.fn().mockResolvedValueOnce({ deleted: true }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
       controllers: [CardsController],
+      providers: [
+        {
+          provide: CardsService,
+          useValue: mockCardService,
+        },
+      ],
     }).compile();
 
-    controller = module.get<CardsController>(CardsController);
+    cardService = module.get<CardsService>(CardsService);
+    cardController = module.get<CardsController>(CardsController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(cardController).toBeDefined();
+  });
+
+  describe('getAllCards', () => {
+    it('should get all cards', async () => {
+      const result = await cardController.getAllCards({
+        page: '1',
+        keyword: 'test',
+      });
+
+      expect(cardService.findAll).toHaveBeenCalled();
+      expect(result).toEqual([mockCard]);
+    });
+  });
+
+  describe('createBook', () => {
+    it('should create a new book', async () => {
+      const newCard = {
+        name: 'Intro Algorithm',
+        status: true,
+        content: 'Lorem ipsum dolor sit amet',
+        category: Category.TECHNOLOGY,
+      };
+
+      mockCardService.create = jest.fn().mockResolvedValueOnce(mockCard);
+
+      const result = await cardController.createCard(
+        newCard as CreateCardDTO,
+        mockUser as User,
+      );
+
+      expect(cardService.create).toHaveBeenCalled();
+      expect(result).toEqual(mockCard);
+    });
+  });
+
+  describe('getCardById', () => {
+    it('should get a card by ID', async () => {
+      const result = await cardController.getCard(mockCard._id);
+
+      expect(cardService.findById).toHaveBeenCalled();
+      expect(result).toEqual(mockCard);
+    });
+  });
+
+  describe('updateCard', () => {
+    it('should update card by its ID', async () => {
+      const updatedCard = { ...mockCard, name: 'Updated name' };
+      const card = { name: 'Updated name' };
+      const req = '64c2c1802a06d43ed9ada955';
+
+      mockCardService.updateById = jest.fn().mockResolvedValueOnce(updatedCard);
+
+      const result = await cardController.updateCard(
+        mockCard._id,
+        card as UpdateCardDTO,
+        req,
+      );
+
+      expect(cardService.updateById).toHaveBeenCalled();
+      expect(result).toEqual(updatedCard);
+    });
+  });
+
+  describe('deleteCard', () => {
+    it('should delete a card by ID', async () => {
+      const req = '64c2c1802a06d43ed9ada955';
+      const result = await cardController.deleteCard(mockCard._id, req);
+
+      expect(cardService.deleteById).toHaveBeenCalled();
+      expect(result).toEqual({ deleted: true });
+    });
   });
 });
--- src/cards/cards.service.spec.ts
+++ src/cards/cards.service.spec.ts
@@ -1,18 +1,187 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import mongoose, { Document, Model } from 'mongoose';
 import { CardsService } from './cards.service';
+import { CreateCardDTO } from './dto/createCard.dto';
+import { Card, Category } from './schemas/card.schema';
+import { ObjectId } from 'mongodb';
+import { User } from 'src/users/schemas/user.schema';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
-describe('CardsService', () => {
-  let service: CardsService;
+describe('BookService', () => {
+  let cardService: CardsService;
+  let model: Model<Card>;
+
+  const id = '64c2c16a2a06d43ed9ada952';
+
+  const myObjectId: ObjectId = ObjectId.createFromHexString(id);
+
+  const mockUser = {
+    _id: myObjectId,
+    username: 'NaufalRDJ',
+    password: 'xcvg5hjbn1237',
+  };
+
+  const mockCard = {
+    _id: '64c2c1802a06d43ed9ada955',
+    name: 'Quantum Physics',
+    status: true,
+    content: 'Lorem ipsum dolor sit amet',
+    category: Category.PHYSICS,
+    user: myObjectId,
+  };
+
+  const mockCardCreate: (Document<unknown, Map<string, string>, Card> &
+    Card & { _id: ObjectId })[] = [];
+
+  const mockCardService = {
+    find: jest.fn(),
+    create: jest.fn(),
+    findById: jest.fn(),
+    findByIdAndUpdate: jest.fn(),
+    findByIdAndDelete: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CardsService],
+      providers: [
+        CardsService,
+        {
+          provide: getModelToken(Card.name),
+          useValue: mockCardService,
+        },
+      ],
     }).compile();
 
-    service = module.get<CardsService>(CardsService);
+    cardService = module.get<CardsService>(CardsService);
+    model = module.get<Model<Card>>(getModelToken(Card.name));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('findAll', () => {
+    it('should return an array of cards', async () => {
+      const query = { page: '1', keyword: 'test' };
+
+      jest.spyOn(model, 'find').mockImplementation(
+        () =>
+          ({
+            limit: () => ({
+              skip: jest.fn().mockResolvedValue([mockCard]),
+            }),
+          } as any),
+      );
+
+      const result = await cardService.findAll(query);
+
+      expect(model.find).toHaveBeenCalledWith({
+        title: { $regex: 'test', $options: 'i' },
+      });
+
+      expect(result).toEqual([mockCard]);
+    });
+  });
+
+  describe('create', () => {
+    it('should create and return a card', async () => {
+      const newCard = {
+        name: 'Intro to algorithm',
+        status: true,
+        content: 'Lorem ipsum dolor sit amet',
+        category: Category.TECHNOLOGY,
+        user: mockUser,
+      };
+
+      jest
+        .spyOn(model, 'create')
+        .mockImplementationOnce(() => Promise.resolve(mockCardCreate));
+
+      const result = await cardService.create(
+        newCard as CreateCardDTO,
+        mockUser as User,
+      );
+
+      expect(result).toEqual([]);
+    });
+  });
+
+  describe('findById', () => {
+    it('should find and return a card by ID', async () => {
+      jest.spyOn(model, 'findById').mockResolvedValue(mockCard);
+
+      const result = await cardService.findById(mockCard._id);
+
+      expect(model.findById).toHaveBeenCalledWith(mockCard._id);
+      expect(result).toEqual(mockCard);
+    });
+
+    it('should throw BadRequestException if invalid ID is provided', async () => {
+      const id = 'invalid-id';
+
+      const isValidObjectIDMock = jest
+        .spyOn(mongoose, 'isValidObjectId')
+        .mockReturnValue(false);
+
+      await expect(cardService.findById(id)).rejects.toThrow(
+        BadRequestException,
+      );
+
+      expect(isValidObjectIDMock).toHaveBeenCalledWith(id);
+      isValidObjectIDMock.mockRestore();
+    });
+
+    it('should throw NotFoundException if card is not found', async () => {
+      jest.spyOn(model, 'findById').mockResolvedValue(null);
+
+      await expect(cardService.findById(mockCard._id)).rejects.toThrow(
+        NotFoundException,
+      );
+
+      expect(model.findById).toHaveBeenCalledWith(mockCard._id);
+    });
+  });
+
+  describe('updateById', () => {
+    it('should update and return a card', async () => {
+      const updatedCard = { ...mockCard, name: 'Updated name' };
+      const card = { name: 'Updated name' };
+
+      jest.spyOn(model, 'findByIdAndUpdate').mockResolvedValue(updatedCard);
+
+      jest.spyOn(model, 'findById').mockResolvedValue(mockCard);
+
+      const findId = await cardService.findById(mockCard._id);
+
+      expect(findId.user._id).toEqual(mockUser._id);
+
+      const result = await cardService.updateById(
+        mockCard._id,
+        card as any,
+        mockUser,
+      );
+
+      expect(model.findByIdAndUpdate).toHaveBeenCalledWith(mockCard._id, card, {
+        new: true,
+        runValidators: true,
+      });
+
+      expect(result.name).toEqual(card.name);
+    });
+  });
+
+  describe('deleteById', () => {
+    it('should delete and return a card', async () => {
+      jest.spyOn(model, 'findByIdAndDelete').mockResolvedValue(mockCard);
+
+      jest.spyOn(model, 'findById').mockResolvedValue(mockCard);
+
+      const findId = await cardService.findById(mockCard._id);
+
+      expect(findId.user._id).toEqual(mockUser._id);
+
+      const result = await cardService.deleteById(mockCard._id, mockUser);
+
+      expect(model.findByIdAndDelete).toHaveBeenCalledWith(mockCard._id);
+
+      expect(result).toEqual(mockCard);
+    });
   });
 });
