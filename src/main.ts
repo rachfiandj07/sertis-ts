49c683bb585d8e090bf1527a460ffa7a3c5223f5
Muhammad Naufal Rachfian dJ <naufalrdj@gmail.com>
2023-07-30 18:35:19 +0700

    feat: move port into .env

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
-  await app.listen(3000);
+  await app.listen(`${process.env.PORT}`);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
